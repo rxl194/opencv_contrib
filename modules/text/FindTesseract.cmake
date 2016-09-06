41ae33f3224c41558780431861a4ed28701ce32e
rxl194 <Rendong_Lin@yahoo.com>
2016-09-06 01:26:09 +0000

    .

--- modules/text/FindTesseract.cmake
+++ modules/text/FindTesseract.cmake
@@ -1,10 +1,12 @@
 # Tesseract OCR
 unset(Tesseract_FOUND)
 
-set(MINGW_DIR C:/MinGW/mingw32)
-set(MINGW_LIB_DIR ${MINGW_DIR}/lib)
-set(LEPTONICA_DIR D:/git/share)
-
+if(MINGW)
+  set(MINGW_DIR C:/MinGW/mingw32)
+  set(MINGW_LIB_DIR ${MINGW_DIR}/lib)
+  set(LEPTONICA_DIR D:/git/share)
+endif()
+  
 find_path(Tesseract_INCLUDE_DIR tesseract/baseapi.h
   HINTS
   /usr/include
@@ -21,13 +23,17 @@ find_library(Lept_LIBRARY NAMES lept
   /usr/lib
   /usr/local/lib)
 
-find_library(WS2_32_LIB NAMES libws2_32.a
+if(MINGW)
+  find_library(WS2_32_LIB NAMES libws2_32.a
                               PATHS 
                               ${MINGW_LIB_DIR}
                               NO_DEFAULT_PATH
                               NO_SYSTEM_ENVIRONMENT_PATH)
+  set(Tesseract_LIBS ${Tesseract_LIBRARY} ${Lept_LIBRARY} ${WS2_32_LIB})
+else()
+  set(Tesseract_LIBS ${Tesseract_LIBRARY} ${Lept_LIBRARY})
+endif()
 
-set(Tesseract_LIBS ${Tesseract_LIBRARY} ${Lept_LIBRARY} ${WS2_32_LIB})
 if(Tesseract_LIBS AND Tesseract_INCLUDE_DIR)
     set(Tesseract_FOUND 1)
 endif()
